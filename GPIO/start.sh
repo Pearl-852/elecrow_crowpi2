e14e056e7fc784330c1feff0df4afc22117062cc
Pearl-852 <74016222+Pearl-852@users.noreply.github.com>
2021-02-26 23:21:49 +0800

    Update start.sh
    Unifies settings with the CrowPi2 image.

--- GPIO/start.sh
+++ GPIO/start.sh
@@ -4,13 +4,13 @@
 #Description:	this script function is configure the file to the new system
 #Version:	1.0
 
-FOLDER="/usr/share/crowpi2"
+FOLDER="/usr/share/elecrow/script"
 mkdir ${FOLDER} &>>error.txt
-cp ./GPIO.py ${FOLDER}
+cp ./shutdown.py ${FOLDER}
 
-RESULT=$(grep "GPIO.py" /etc/rc.local)
+RESULT=$(grep "shutdown.py" /etc/rc.local)
 if [ -z "${RESULT}" ]; then
-	sed -i 's@fi@&\npython /usr/share/crowpi2/GPIO.py@' /etc/rc.local
+	sed -i 's@fi@&\npython /usr/share/elecrow/script/shutdown.py@' /etc/rc.local
 else
 	echo "Script has been added!"
 fi
